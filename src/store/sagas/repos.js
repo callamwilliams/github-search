7cdd2888b6c5e2de3098a3c46c7ce69d90388516
Callam Williams <lhocdesign@gmail.com>
2019-06-29 23:35:27 +0100

    [base] - remove redundant funcs

--- src/store/sagas/repos.js
+++ src/store/sagas/repos.js
@@ -3,21 +3,6 @@ import axios from 'axios';
 import changeCase from 'change-case';
 import { GET_REPOS_SUCCESS, GET_REPOS_FAIL } from '../constants/actionTypes';
 
-function cleanURL(url) {
-  let cleanLink = decodeURIComponent(url);
-  cleanLink = cleanLink.split(',').find(s => s.indexOf('rel="next"') > -1);
-  cleanLink = cleanLink.split(';')[0].slice(1, -1);
-  return cleanLink;
-}
-
-function getNextPage(response) {
-  const link = response.headers.link;
-  if (!link) return null;
-  const nextLink = cleanURL(link);
-  if (!nextLink) return null;
-  return nextLink;
-}
-
 function getUrlType(username, userType) {
   let url;
   if (userType === 'username') {
